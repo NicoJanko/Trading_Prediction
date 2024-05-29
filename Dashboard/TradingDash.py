fce26e1a21383a1667f96207bdf6d8b963e79a3a
Morkith <nicolas.jankovsky@gmail.com>
2024-05-29 18:27:05 +0200

    requierements issues

--- Dashboard/TradingDash.py
+++ Dashboard/TradingDash.py
@@ -1,5 +1,7 @@
 from dash import Dash, html, dcc, callback, Output, Input
 from dash_auth import BasicAuth
+import psycopg2 as psg2
+
 
 
 VALID_USERNAME_PASSWORD_PAIRS = {
@@ -19,9 +21,16 @@ auth = BasicAuth(
 
 
 app.layout = html.Div([
-    html.H1('Hello, Dash!'),
-    html.Div('This is a simple Dash dashboard with basic authentication.')
+    html.H1('Trading Dash', style={'textAlign':'center'}),
+   dcc.Dropdown(stock_name, id)'dropdown'),
+   dcc.Graph(id='graph')
 ])
+
+@callback(
+    Output('graph','figure'),
+    Input('dropdown','value')
+    )
+def update_graph
 
 if __name__ == '__main__':
     app.run_server(debug=True, host = "0.0.0.0", port=8080)
